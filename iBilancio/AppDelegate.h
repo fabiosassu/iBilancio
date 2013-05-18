25cfdd87d721f19f3b8b04900916286a0a8ca912
Fabio Sassu <fabio_figo71@MacBook-di-Fabio-Sassu.local>
2013-05-18 18:45:13 +0200

    Initial Commit

---
+++ iBilancio/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iBilancio
+//
+//  Created by Fabio Sassu on 18/05/13.
+//  Copyright (c) 2013 Fabio Sassu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iBilancio/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iBilancio
+//
+//  Created by Fabio Sassu on 18/05/13.
+//  Copyright (c) 2013 Fabio Sassu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
